d9bcab37773ba9255cc8033720d165116fc61a6b
Azhad101 <withazhad@gmail.com>
2026-04-30 23:46:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.fromTo(
+    ".loading-page", 
+    {opacity:1},
+    {
+    opacity:0,
+    duration:1.5,
+    delay:3.5,
+})
+
+gsap.fromTo(
+    ".loading-name",
+    {
+    y:50,
+    opacity:0
+},
+{
+    y:0,
+    opcaity:1,
+    duration:2,
+    delay:0.5,
+})
